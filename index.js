c46fcb4bed3fc38ade264366748d7db4bf2b924d
ferdouszihad <f.zihad333@gmail.com>
2024-12-19 02:50:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+//initialization start ++++++++++++++++++++++++++++++++++++++++++++++++
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+//initialization end ------------------------------------------
+
+// middleware start ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// middleware end ----------------------------------------------------
+
+async function run() {
+  try {
+    //test connection +++++++++++++++++++++++++++++++++++++++++++
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("successfully connected to MongoDB!");
+    //test connection -------------------------------------------
+
+    //Database info +++++++++++++++++++++++++++++++++++++++++++++
+    const database = client.db("classroomDB");
+    const studentCollection = database.collection("students");
+    //Database info ------------------------------------------
+
+    // API Start ++++++++++++++++++++++++++++++++++++++++++++++
+
+    // GET - all students
+    app.get("/students", async (req, res) => {
+      const result = await studentCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Add Bulk Data {Run it only once}
+    const data = require("./data.json");
+    console.log(data);
+    app.get("/add-student-script", async (req, res) => {
+      const result = await studentCollection.deleteMany();
+      res.send(result);
+    });
+
+    // API End ------------------------------------------------
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server Running on (${port})`);
+});
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Simple Class Room Server
+
+Do Following Things to run this Server
+
+### 1 - clone the Repo
+
+### 2 - Create new DB-User and DB-PASS
+
+### 3 - Create .env Add your URI on .env
+
+```bash
+#demo example
+URI=mongodb+srv://USER:PASS@CLUSTERLINK
+```
+
+### 4 - install dependencies
+
+```bash
+npm install
+```
+
+### 5 - Run Server
+
+```bash
+nodemon index.js
+```
+
+or you can also use this
+
+```bash
+npm run dev
+```
